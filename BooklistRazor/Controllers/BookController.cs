5128934a684ec44214396540e5a5e2246d390343
ifrat Mitul <ifrat.mitul@hotmail.com>
2020-07-09 09:09:48 +0600

    api calling added with controller

---
+++ BooklistRazor/Controllers/BookController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BooklistRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace BooklistRazor.Controllers
+{
+    [Route("api/Book")]
+    [ApiController]
+    public class BookController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public BookController(ApplicationDbContext db)
+        {
+            _db = db;
+
+        }
+        
+        [HttpGet]
+
+        public IActionResult GetAll()
+        {
+            return Json(new { data = _db.Book.ToList() });
+            
+        }
+    }
+}
--- BooklistRazor/Startup.cs
+++ BooklistRazor/Startup.cs
@@ -25,7 +25,8 @@ namespace BooklistRazor
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(option => option.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))); 
+            services.AddDbContext<ApplicationDbContext>(option => option.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddControllersWithViews();
             services.AddRazorPages().AddRazorRuntimeCompilation();
         }
 
@@ -52,6 +53,7 @@ namespace BooklistRazor
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllers();
                 endpoints.MapRazorPages();
             });
         }
